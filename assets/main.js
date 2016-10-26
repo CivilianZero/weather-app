15293b7ee36390b8b961e1b0c6722cbdc30a77db
CivilianZero <Civilian.Protocol@gmail.com>
2016-10-26 17:09:58 -0400

    basic functionality complete

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,11 @@
-var data;
-// http request
+var dataArray;
+// http request function
 $.ajax({
     url: 'http://api.openweathermap.org/data/2.5/forecast/daily?lat=7.899&lon=98.4&cnt=10&mode=json&appid=464dd702fe9c8db2e541721da7d8822a',
-    // method: 'GET',
     success: function (results) {
-        data = results;
+        dataArray = results;
+        var overview = new Overview('main', dataArray);
+        overview.render();
     }
 });
 
@@ -39,25 +40,69 @@ function Overview () {
 
 // overview functions and prototype
 Overview.prototype = Object.create(View.prototype);
-Overview.prototype.render = function () {};
+Overview.prototype.render = function () {
+    console.log(this);
+    
+    var { city: {name} } = this.data;
+    $('h1').text(name);
+
+    this.data.list.forEach(function (value) {
+        var daily = new ForecastView('div', value);
+
+        daily.render();
+        $('main').append(daily);
+    });
+};
+
 Overview.prototype.bindEvents = function () {};
 
-// create Daily View
-function Daily () {
+// create ForecastView View
+function ForecastView () {
     View.apply(this, arguments);
 }
 
 //daily functions and prototype
-Daily.prototype = Object.create(View.prototype);
-Daily.prototype.render = function () {};
-Daily.prototype.bindEvents = function () {};
+ForecastView.prototype = Object.create(View.prototype);
+ForecastView.prototype.render = function () {
+    var { speed, deg, temp: {min, max} } = this.data,
+        { weather: [{main}]} = this.data,
+        direction = cardinal(deg),
+        d = new Date(),
+        dayN = d.getDay(),
+        daysOfWeek = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN'],
+        currentDay = daysOfWeek[dayN],
+        date = d.getDate();
 
 
+    min = convertTemp(min);
+    max = convertTemp(max);
+    var htmlString = `<div class="report">
+            <div class="left">
+                <div class="date">  
+                    <h2>${currentDay}</h2>
+                    <h2>${date}</h2>
+                </div>
+                <div class="conditions-small">
+                    <img class="" src="">
+                    <h4 class="">COND</h4>
+                </div>
+            </div>
+            <div class="right">
+                <div class="conditions-large">
+                    <img src="">
+                    <h3>CONDITIONS</h3>
+                </div>
+                <div class="day">
+                    <h2>DAY</h2>
+                </div>
+                <div class="data">
+                    <h4 class="wind">WIND: ${speed} ${direction}</h4>
+                    <h4 class="hi">HI: ${max}</h4>
+                    <h4 class="lo">LO: ${min}</h4>
+                </div>  
+            </div>
+        </div>`;
+    $('main').append(htmlString);
+};
 
-
-
-
-
-
-
-
+ForecastView.prototype.bindEvents = function () {};
